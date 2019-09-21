71bff4d505ce5890f4e06357cf621dfd86342b64
Manu Ferrer <38438207+ManuFerrer094@users.noreply.github.com>
2019-09-22 01:29:52 +0200

    WIP: Ruteo establecido

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Minavbar from './Minavbar'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Homepage from './Homepage';
+import Tareas from './Tareas';
+import Contacto from './Contacto';
 
-function App() {
-  return (
-    <div className="App">
-      <Minavbar></Minavbar>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Minavbar></Minavbar>
+ 
+          <Switch>
+            <Route path="/homepage" component={Homepage}/> 
+            <Route path="/tareas" component={Tareas} />
+            <Route path="/contacto" component={Contacto} />
+          </Switch>
+ 
+         
+        </div>
+      </Router>
+    );
+  }
 }
-
+ 
 export default App;
